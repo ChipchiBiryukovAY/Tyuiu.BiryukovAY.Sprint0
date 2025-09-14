06ad9da6d635635c200603be74f08a89d341f227
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-14 19:46:22 +0700

    Task2

---
+++ Tyuiu.BiryukovAY.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BiryukovAY.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BiryukovAY.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9a0a94e-b814-4412-89dd-a1a258d1888b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BiryukovAY.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BiryukovAY.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BiryukovAY.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessage
+            //из библиотеки Tyuiu.BiryukovAY.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Алексей"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
