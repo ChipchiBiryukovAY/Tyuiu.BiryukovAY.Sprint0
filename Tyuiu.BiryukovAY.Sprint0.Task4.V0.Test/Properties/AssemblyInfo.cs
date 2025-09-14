258e7651d5a949d3ac2b970cfb101fbc9212d4b0
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-14 20:42:07 +0700

    Task4

---
+++ Tyuiu.BiryukovAY.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BiryukovAY.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BiryukovAY.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c98f7d09-e27f-4da8-81b0-aef90ec80fa9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BiryukovAY.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BiryukovAY.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.BiryukovAY.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры программы
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            //Пример линейной структуры программы
+            //Вызов метода вычитания Subtraction
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            //Вызов метода умножения Multiplication
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //Пример линейной структуры программы
+            //Вызов метода деления Division
+            Console.WriteLine(DataService.Division(5, 5));
+
+
+            Console.ReadKey();
+            }
+        }
+    }
