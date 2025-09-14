62c3df6b21aee7a1fb81dae5420b5427a817f5c1
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-14 17:44:09 +0700

    Task1

--- Tyuiu.BiryukovAY.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.BiryukovAY.Sprint0.Task1.V0/Program.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.BiryukovAY.Sprint0.Task1.V0
         {
             Console.WriteLine("Hello, World");
             string name = "Алексей";
-            int age = 48;
+            int age = 49;
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
